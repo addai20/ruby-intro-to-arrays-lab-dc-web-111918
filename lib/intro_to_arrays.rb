48679a9a705e15ccbdf27705bcba794d098196b1
addai.5 <addai.5@buckeyemail.osu.edu>
2018-11-15 16:49:16 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,5 +6,15 @@ def array_with_two_elements
   new_array = ["a", "b"]
 end
 
-def first_element
+def first_element(array)
+  array[0]
+end
+
+def third_element(array)
+  array[2]
+  
+end
+
+def last_element(array)
+  array[-1]
 end
